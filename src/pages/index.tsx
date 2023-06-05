ee0a67f3aba8cd52812a8cbfe69c354521a28620
Ozin69 <102983926+Ozin69@users.noreply.github.com>
2023-06-05 12:07:04 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       <header className='relative'>
         <nav className={`${pageLoad ? 'animate-fade-in-top' : 'opacity-0'} fixed flex w-full z-50 bg-slate-950/80 p-4 sm:px-24 px-4 justify-between items-center`}>
           <Link href='/'>
-            <img draggable={false} className='h-12 w-12 rounded-full' alt="image" src="https://r2.e-z.host/2082d908-7c65-4fc3-b02a-5f50f9141543/lbo1x6wn.png" />
+            <img draggable={false} className='h-12 w-12 rounded-full' alt="image" src="https://images.app.goo.gl/UF9u6KV5BFeeChwm9" />
           </Link>
           <ul className='flex flex-row gap-x-2'>
             <li>
